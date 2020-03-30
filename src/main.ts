c8789e5a28c82436833df610db166c58af084df9
CyberImpXIII <jnelsenepstein@gmail.com>
2020-03-29 21:38:35 -0500

    typo

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors : true});
-  await app.listen(6060);
+  await app.listen(process.env.PORT || 6060);
 }
 bootstrap();
